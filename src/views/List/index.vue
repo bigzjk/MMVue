8ef6d66a551c796b6e4e24be0a07a120314b32aa
alkun <zhujunkun@yit.com>
2019-10-09 17:11:04 +0800

    feat: v-for

---
+++ src/views/List/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="list">
+    <h1>list List</h1>
+    <ul>
+        <li v-for="item in todos" v-bind:key="item" >
+            {{ item.text }}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                todos: [
+                    { text: '学习 JavaScript' },
+                    { text: '学习 Vue' },
+                    { text: '整个牛项目' }
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>script>
